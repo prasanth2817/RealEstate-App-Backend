4d86a80d81bb38a9ff758bf2f8891c7f230fdc43
prasanth2817 <prasanth2817@gmail.com>
2024-10-26 02:40:30 +0530

    added Fronend Deployed Url

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 const PORT= process.env.PORT || 8000
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 app.use(express.json())
 app.use('/',AppRoutes)
 
