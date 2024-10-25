5e0e9c2ec281d4366ae2234024c036e720199209
prasanth2817 <prasanth2817@gmail.com>
2024-10-26 03:14:34 +0530

    fixings cors error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT= process.env.PORT || 8000
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL ,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
